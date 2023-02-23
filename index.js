017301b1a326252029b3a2bd44ac4c1d6257088e
Dhruv Singh Bora <21052561@kiit.ac.in>
2023-02-23 16:11:19 +0530

    dhruv

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-import 
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+const app=express();
+
+let user=[]
+
+app.use(bodyParser.json());
+
+const PORT=5001;
+
+let a={};
+app.post( '/post', (req, res)=> {
+    a=req.body;
+    console.log(a)
+    user.push(...user,a)
+    //res.send(req.body)
+})
+
+app.get ('/post/:id', (req, res)=>{
+    id = req.params;
+    res.send(user.find((user)=> user.id))
+})
+
+app.delete('/post/:id', (req, res) => {
+    // Handle the request
+    const id = req.params.roll ;
+    console.log(`Deleting resource ${id}`);
+    res.send(`Deleted resource ${id}`);
+  });
+
+app.listen(PORT, () => console.log(`Server running on port : http://localhost:${PORT}`));
